216e073f43faa9e97ffd157e044d5b5dc297b91e
ramzibouzaiene <ramzibouzaiene00@gmail.com>
2023-05-21 15:33:11 -0700

    adding routes for the application setup note form and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Navigate, Route, Routes } from "react-router-dom";
+import NewNote from "./NewNote";
+
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type Tag = {
+  id: string;
+  label: string;
+};
 function App() {
-
   return (
-    <div>
-
-    </div>
-  )
+    <Container>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
 }
 
-export default App
+export default App;
