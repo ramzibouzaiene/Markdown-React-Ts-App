e04c8832c3827ac2f8918918c1060202731a08ec
ramzibouzaiene <ramzibouzaiene00@gmail.com>
2023-05-21 16:08:51 -0700

    creating custom hook for local storage creating the login for creating new note

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { Navigate, Route, Routes } from "react-router-dom";
 import NewNote from "./NewNote";
+import { useLocalStorage } from "./useLocalStorage";
+import { useMemo } from "react";
+import { v4 as uuidV4 } from "uuid";
+export type RawNote = {
+  id: string;
+} & RawNoteData;
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
 
 export type Note = {
   id: string;
@@ -18,11 +30,31 @@ export type Tag = {
   label: string;
 };
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTE", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
+  const noteWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes((prevNotes) => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map((tag) => tag.id) },
+      ];
+    });
+  }
   return (
     <Container>
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/new" element={<NewNote />} />
+        <Route path="/new" element={<NewNote onSubmit={onCreateNote} />} />
         <Route path="/:id">
           <Route index element={<h1>Show</h1>} />
           <Route path="edit" element={<h1>Edit</h1>} />
