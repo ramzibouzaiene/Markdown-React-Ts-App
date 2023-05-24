2f9799d146a6b9f14837c5632e880a8e7b7f6860
ramzibouzaiene <ramzibouzaiene00@gmail.com>
2023-05-23 22:35:53 -0700

    adding edit and delete functionality

--- src/Note.tsx
+++ src/Note.tsx
@@ -1,10 +1,15 @@
 import { Badge, Button, Col, Row, Stack } from "react-bootstrap";
 import { useNote } from "./NoteLayout";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
 
-export default function Note() {
+type NoteProps = {
+  onDelete: (id: string) => void;
+};
+
+export default function Note({ onDelete }: NoteProps) {
   const note = useNote();
+  const navigate = useNavigate();
 
   return (
     <>
@@ -26,7 +31,15 @@ export default function Note() {
             <Link to={`/${note.id}/edit`}>
               <Button variant="primary">Edit</Button>
             </Link>
-            <Button variant="outline-danger">Delete</Button>
+            <Button
+              onClick={() => {
+                onDelete(note.id);
+                navigate("/");
+              }}
+              variant="outline-danger"
+            >
+              Delete
+            </Button>
             <Link to="/">
               <Button variant="outline-secondary">Back</Button>
             </Link>
